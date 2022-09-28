1bb52c874951873e9bd9122aa17f1252f8a6247a
hagai arad <hagaya@hagays-MacBook-Pro.local>
2022-09-28 12:31:46 +0300

    post action to run the notebook

--- app/main.py
+++ app/main.py
@@ -13,3 +13,10 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("activate_python_notebook")
+def activate_python_notebook(notebook_source: str, notebook_target: str, parameters: list):
+    # TODO: activate the notebook using papermill and return the output
+
+    return None
