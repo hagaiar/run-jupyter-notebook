77180a2197bee15bf95b844d53be03cac9c6cbbd
hagai arad <hagaya@hagays-MacBook-Pro.local>
2022-09-28 14:27:41 +0300

    activate_python_nb with params endpoint

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 from typing import Union
-
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+import papermill as pm
 
 app = FastAPI()
 
+class Nb_params(BaseModel):
+    limit: int = 1000
+    country_limit: int = 15
 
 @app.get("/")
 def read_root():
@@ -15,8 +20,18 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
-@app.post("activate_python_notebook")
-def activate_python_notebook(notebook_source: str, notebook_target: str, parameters: list):
-    # TODO: activate the notebook using papermill and return the output
+@app.post("/activate_python_notebook")
+def activate_python_notebook(parameters_for_notebook: Nb_params
+    , notebook_source: str='s3://aiola-469190457957-h-playground/misc/num_flights_by_country.ipynb'
+    , notebook_target: str='s3://aiola-469190457957-h-playground/misc/jupyter_notebooks_outputs/num_of_flights_by_country.ipynb'):
 
-    return None
+    dict_of_params = dict(parameters_for_notebook)
+
+    pm.execute_notebook(
+        notebook_source,
+        notebook_target,
+        parameters=dict(limit=dict_of_params['limit']
+            , country_limit=dict_of_params['country_limit'])
+    )
+
+    return "done"
